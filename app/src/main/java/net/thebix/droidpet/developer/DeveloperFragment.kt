086905014603b560d128deb6ffa67245a7856278
Pavel Biriukov <syntheticforms@gmail.com>
2018-06-24 21:20:19 +0300

    added navigation to Github Reposlist from Developer Fragment

--- app/src/main/java/net/thebix/droidpet/developer/DeveloperFragment.kt
+++ app/src/main/java/net/thebix/droidpet/developer/DeveloperFragment.kt
@@ -5,9 +5,20 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.disposables.CompositeDisposable
 import net.thebix.droidpet.R
+import net.thebix.droidpet.developer.navigation.DeveloperFragmentNavigator
+import net.thebix.droidpet.navigation.NavigationManager
 
 class DeveloperFragment : Fragment() {
+
+    private val buttonRepoList get() = view!!.findViewById(R.id.fragment_developer_repo_list_button) as Button
+
+    // TODO: DeveloperFragmentNavigator (as NavigationManager) should be injected
+    private val navigationManager: DeveloperFragmentNavigator = NavigationManager()
+    private lateinit var disposables: CompositeDisposable
 
     companion object {
         fun newInstance(): DeveloperFragment = DeveloperFragment()
@@ -15,6 +26,21 @@ class DeveloperFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_developer, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        disposables = CompositeDisposable(
+            RxView.clicks(buttonRepoList)
+                .subscribe {
+                    fragmentManager?.let { navigationManager.goToRepolist(it) }
+                }
+        )
+    }
+
+    override fun onStop() {
+        disposables.clear()
+        super.onStop()
     }
 
 }
