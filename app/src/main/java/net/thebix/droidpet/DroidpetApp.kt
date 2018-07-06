3c59934092bac818de0a2cba0d676fed0909d978
Pavel Biriukov <syntheticforms@gmail.com>
2018-07-06 10:09:39 +0300

    added application Dagger component

--- app/src/main/java/net/thebix/droidpet/DroidpetApp.kt
+++ app/src/main/java/net/thebix/droidpet/DroidpetApp.kt
@@ -1,14 +1,30 @@
 package net.thebix.droidpet
 
+import android.app.Activity
 import android.app.Application
+import net.thebix.droidpet.github.api.di.ContextModule
+import net.thebix.droidpet.github.api.di.DaggerGithubComponent
+import net.thebix.droidpet.github.api.di.GithubComponent
 import timber.log.Timber
 
+@Suppress("unused")
 class DroidpetApp : Application() {
+
+    private lateinit var droidpetAppComponent: GithubComponent
+
+    companion object {
+        fun get(activity: Activity): DroidpetApp = activity.application as DroidpetApp
+    }
 
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        droidpetAppComponent = DaggerGithubComponent.builder()
+            .contextModule(ContextModule(this))
+            .build()
     }
+
+    fun getDroidpetAppComponent(): GithubComponent = droidpetAppComponent
 }
