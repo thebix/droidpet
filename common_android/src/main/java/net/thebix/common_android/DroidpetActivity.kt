fcd8b46b512ffba799113d4350d540d95b95186e
Pavel Biriukov <syntheticforms@gmail.com>
2018-08-01 20:22:09 +0300

    implemented android-module common_android

---
+++ common_android/src/main/java/net/thebix/common_android/DroidpetActivity.kt
@@ -0,0 +1,21 @@
+@file:Suppress("PackageName")
+
+package net.thebix.common_android
+
+import android.support.v7.app.AppCompatActivity
+import net.thebix.common.DroidpetComponent
+import net.thebix.common.DroidpetComponentBuilder
+import net.thebix.common.HasDroidpetSubcomponentBuilders
+import javax.inject.Inject
+import javax.inject.Provider
+
+abstract class DroidpetActivity : AppCompatActivity(),
+                                  HasDroidpetSubcomponentBuilders {
+    @Inject
+    lateinit var droidpetComponentBuilders: Map<Class<out DroidpetComponent>, @JvmSuppressWildcards Provider<DroidpetComponentBuilder<*>>>
+
+    override fun getDroidpetComponentBuilder(droidpetComponentClass: Class<out DroidpetComponent>): DroidpetComponentBuilder<*> {
+        return droidpetComponentBuilders[droidpetComponentClass]!!.get()
+    }
+
+}
